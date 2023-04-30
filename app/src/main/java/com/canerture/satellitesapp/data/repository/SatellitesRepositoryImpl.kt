3bb9aacda0010623f56047800a8c598f15cf7fc8
Caner Türe <cnrture@gmail.com>
2023-04-30 18:17:11 +0300

    Created RepositoryModule, added domain package

---
+++ app/src/main/java/com/canerture/satellitesapp/data/repository/SatellitesRepositoryImpl.kt
@@ -0,0 +1,13 @@
+package com.canerture.satellitesapp.data.repository
+
+import com.canerture.satellitesapp.data.datasource.json.JsonDataSource
+import com.canerture.satellitesapp.data.datasource.room.RoomDataSource
+import com.canerture.satellitesapp.domain.SatellitesRepository
+import javax.inject.Inject
+
+class SatellitesRepositoryImpl @Inject constructor(
+    private val jsonDataSource: JsonDataSource,
+    private val roomDataSource: RoomDataSource
+) : SatellitesRepository {
+
+}
---
+++ app/src/main/java/com/canerture/satellitesapp/di/RepositoryModule.kt
@@ -0,0 +1,23 @@
+package com.canerture.satellitesapp.di
+
+import com.canerture.satellitesapp.data.datasource.json.JsonDataSource
+import com.canerture.satellitesapp.data.datasource.room.RoomDataSource
+import com.canerture.satellitesapp.data.repository.SatellitesRepositoryImpl
+import com.canerture.satellitesapp.domain.SatellitesRepository
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object RepositoryModule {
+
+    @Provides
+    @Singleton
+    fun provideSatelliteRepository(
+        jsonDataSource: JsonDataSource,
+        roomDataSource: RoomDataSource
+    ): SatellitesRepository = SatellitesRepositoryImpl(jsonDataSource, roomDataSource)
+}
