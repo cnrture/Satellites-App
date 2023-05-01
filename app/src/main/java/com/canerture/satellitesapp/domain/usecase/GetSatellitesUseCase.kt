1bedfff68038ef6cb1d3d61f98f7082119598516
Caner Türe <cnrture@gmail.com>
2023-05-01 06:28:31 +0300

    Created GetSatellitesUseCase

---
+++ app/src/main/java/com/canerture/satellitesapp/domain/usecase/GetSatellitesUseCase.kt
@@ -0,0 +1,35 @@
+package com.canerture.satellitesapp.domain.usecase
+
+import com.canerture.satellitesapp.common.Resource
+import com.canerture.satellitesapp.data.model.Satellite
+import com.canerture.satellitesapp.domain.repository.SatellitesRepository
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+import javax.inject.Inject
+
+class GetSatellitesUseCase @Inject constructor(
+    private val satellitesRepository: SatellitesRepository
+) {
+    operator fun invoke(): Flow<GetSatellitesUseCaseState> = flow {
+
+        when (val result = satellitesRepository.getSatellites()) {
+            is Resource.Success -> {
+                if (result.data.isEmpty()) {
+                    emit(GetSatellitesUseCaseState.EmptyList)
+                } else {
+                    emit(GetSatellitesUseCaseState.Satellites(result.data))
+                }
+            }
+
+            is Resource.Error -> {
+                emit(GetSatellitesUseCaseState.Error(result.message))
+            }
+        }
+    }
+
+    sealed class GetSatellitesUseCaseState {
+        data class Satellites(val satellites: List<Satellite>) : GetSatellitesUseCaseState()
+        data class Error(val message: String) : GetSatellitesUseCaseState()
+        object EmptyList : GetSatellitesUseCaseState()
+    }
+}
